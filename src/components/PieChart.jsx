ddbd73df18d6ac68ef9ad393e2e1f41a22683978
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2024-06-03 01:20:31 +0530

    UI

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,12 +1,24 @@
 
 import React from 'react';
-import { PieChart, Pie, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Tooltip, Legend, ResponsiveContainer, Cell } from 'recharts';
+
+const colors = {
+  'Severity 1': '#E2725B',
+  'Severity 2': '#FFA501', 
+  'Severity 3': '#FF0000', 
+  'Severity 4': '#008000', 
+  'Severity 5': '#0000FF',
+};
 
 const PieChartComponent = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <PieChart>
-        <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={110} fill="#E2725B" label />
+        <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={110} label >
+        {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[entry.name]} />
+          ))}
+          </Pie>
         <Tooltip />
         <Legend />
       </PieChart>
